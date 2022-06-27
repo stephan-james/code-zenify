d2cd3f4afd00fd2a8366639b94bf1c124d5816cb
GitHub Action <action@github.com>
2022-06-27 15:16:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/stephanjames/desjdzenify/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/stephanjames/desjdzenify/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.stephanjames.desjdzenify.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stephanjames.desjdzenify.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/stephanjames/desjdzenify/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stephanjames/desjdzenify/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.stephanjames.desjdzenify.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stephanjames.desjdzenify.MyBundle
 
 class MyProjectService(project: Project) {
 
